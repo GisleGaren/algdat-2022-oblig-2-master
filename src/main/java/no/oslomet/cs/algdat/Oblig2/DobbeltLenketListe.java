bedf1aefbd7e0f381605036f982ba734a3fd8ef6
GisleGaren <89572928+GisleGaren@users.noreply.github.com>
2022-10-07 14:19:23 +0200

    Ferdig med 3a) finnNode(), hent(), oppdater() er gjort.

--- src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
@@ -158,7 +158,7 @@ public class DobbeltLenketListe<T> implements Liste<T> {
         Node<T> p = finnNode(indeks);
         T gammelVerdi = p.verdi;
 
-        p.verdi = nyverdi; // kommentarer
+        p.verdi = nyverdi; // kommentareradsfadsf
         return gammelVerdi;
     }
 
